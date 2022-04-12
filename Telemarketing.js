6bb84db69cc9987e11e96b118dee95170274f8b0
Andre-Marqs <69987560+Andre-Marqs@users.noreply.github.com>
2022-04-12 19:27:01 -0300

    Create Telemarketing.js

---
+++ Telemarketing.js
@@ -0,0 +1,45 @@
+let = menu
+
+
+menu = Number(prompt(`Escolha a oparação que deseja fazer \n 1 - Atendente \n 2 - Rh \n 3 - Gerente \n 4 - Sair`))
+
+
+
+    switch (menu){
+        case 1:
+         alert ("Abrindo chat com Atendente")
+         if (1){
+            menu = Number(prompt(`Escolha a oparação que deseja fazer \n 1 - Atendente \n 2 - Rh \n 3 - Gerente \n 4 - Sair`))
+         }
+    
+        break
+       
+       
+       
+       case 2:
+        alert ("Abrindo chat com Rh")
+       if(2){
+        menu = Number(prompt(`Escolha a oparação que deseja fazer \n 1 - Atendente \n 2 - Rh \n 3 - Gerente \n 4 - Sair`))
+       }
+        break
+       
+       
+       
+       case 3:
+        alert ("brindo chat com Gerente")
+       if(3){
+        menu = Number(prompt(`Escolha a oparação que deseja fazer \n 1 - Atendente \n 2 - Rh \n 3 - Gerente \n 4 - Sair`))
+       }
+         break
+       
+       
+       
+       case 4: 
+        alert ("Saindo do sistemesa")
+       
+        
+
+
+
+       }           
+       
